cd26bde74d0d3c9e59c3ed7912dc3d0b0ad908d1
Nardos Mamo <nmamo@ithaca.edu>
2023-04-02 11:03:19 -0400

    Wrote method function header for medication class

--- src/main/java/edu/ithaca/barr/meds/Medication.java
+++ src/main/java/edu/ithaca/barr/meds/Medication.java
@@ -2,4 +2,35 @@ package edu.ithaca.barr.meds;
 
 public class Medication {
     
+    private String name;
+    private int id;
+    private double dosage;
+    private int frequency;
+    
+    public Medication(String name, int id, double dosage, int frequency) {
+        // Constructor code goes here
+    }
+
+    public String getName() {
+        // Getter code for name goes here
+
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+    
+    public int getId() {
+        // Getter code for id goes here
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+    
+    public double getDosage() {
+        // Getter code for dosage goes here
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+    
+    public int getFrequency() {
+        // Getter code for frequency goes here
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    
 }

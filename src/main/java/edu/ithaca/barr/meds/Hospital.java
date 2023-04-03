d4f53c82138293ec3050c95f64b334486afddc66
Nardos Mamo <nmamo@ithaca.edu>
2023-04-02 22:18:52 -0400

    Wrote implmentation for hospital class

--- src/main/java/edu/ithaca/barr/meds/Hospital.java
+++ src/main/java/edu/ithaca/barr/meds/Hospital.java
@@ -1,5 +1,23 @@
 package edu.ithaca.barr.meds;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Hospital {
     
+    ArrayList<Medication> medications;
+
+    public Hospital() {
+        this.medications = new ArrayList();
+    }
+
+
+public ArrayList<Medication> getMedications(){
+    throw new UnsupportedOperationException("Not implemented yet.");
+}
+
+public Medication searchMedication(int id){
+    throw new UnsupportedOperationException("Not implemented yet.");
+}
+
 }

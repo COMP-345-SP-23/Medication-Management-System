7cd1409903f3fb302a9b24d0f813a9d3ca27429a
simre <smelak@ithaca.edu>
2023-04-02 23:38:01 -0400

    wrote test for add delete and update methods for the doctor class

--- src/test/java/edu/ithaca/barr/meds/DoctorTest.java
+++ src/test/java/edu/ithaca/barr/meds/DoctorTest.java
@@ -1,5 +1,36 @@
 package edu.ithaca.barr.meds;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class DoctorTest {
-    
+
+    @Test
+    public void testAddMedication() {
+        Doctor doctor = new Doctor();
+        doctor.addMedication("Ibuprofen", 12345, 200.0, 2);
+        assertEquals(1, doctor.hospital.getMedications().size());
+    }
+
+    @Test
+    public void testDeleteMedication() {
+        Doctor doctor = new Doctor();
+        doctor.addMedication("Ibuprofen", 12345, 200.0, 2);
+        doctor.deleteMedication(12345);
+        assertEquals(0, doctor.hospital.getMedications().size());
+    }
+
+ 
+
+    @Test
+    public void testUpdateMedication() {
+        Doctor doctor = new Doctor();
+        doctor.addMedication("Ibuprofen", 12345, 200.0, 2);
+        doctor.updateMedication("Aspirin", 12345, 300.0, 3);
+        Medication medication = doctor.hospital.searchMedication(12345);
+        assertEquals("Aspirin", medication.getName());
+        assertEquals(300.0, medication.getDosage(), 0.001);
+        assertEquals(3, medication.getFrequency());
+    }
 }

277231a4245019cdfceaa36c3417c205ac580c83
simre <smelak@ithaca.edu>
2023-04-02 23:38:40 -0400

    wrote add,delete and update methods for doctor class

--- src/main/java/edu/ithaca/barr/meds/Doctor.java
+++ src/main/java/edu/ithaca/barr/meds/Doctor.java
@@ -2,4 +2,25 @@ package edu.ithaca.barr.meds;
 
 public class Doctor {
     
+    Hospital hospital = new Hospital();
+
+    public void addMedication(String name , int id , double dosage, int frequency){
+        Medication medication = new Medication(name,id,dosage,frequency);
+        hospital.medications.add(medication);
+    }
+
+    public void deleteMedication(int medId){
+       if(hospital.searchMedication(medId)!= null)
+            hospital.getMedications().removeIf(med -> med.getId()==medId);
+       else
+            throw new IllegalArgumentException("A medication with this ID does not exist");        
+    }
+
+   
+    //deletes the medication with the indicated Id and creates another one with the same id
+    public void updateMedication(String name, int id, double dosage, int frequency){
+        deleteMedication(id);
+        addMedication(name, id, dosage, frequency);
+
+    }
 }

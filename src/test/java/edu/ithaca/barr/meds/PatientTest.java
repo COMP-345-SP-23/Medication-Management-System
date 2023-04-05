e2977c85b5374c2173f56b1b85eb56e78e1b575b
Ryan Puorro <rpuorro@ithaca.edu>
2023-04-05 11:31:10 -0400

    wrote isPrescribedTest

--- src/test/java/edu/ithaca/barr/meds/PatientTest.java
+++ src/test/java/edu/ithaca/barr/meds/PatientTest.java
@@ -30,13 +30,21 @@ public class PatientTest {
 
         Patient p1 = new Patient("patient", "smith", 1, "patient@a.com", "password");
         p1.requestMedication(advil);
-        assertTrue(p1.patientMeds.contains(advil));
-        assertFalse(p1.patientMeds.contains(tylenol));
+        assertTrue(p1.currentMeds.contains(advil));
+        assertFalse(p1.currentMeds.contains(tylenol));
     }
 
     @Test
     public void isPrescribedTest() {
+        Hospital hospital = new Hospital();
+        Medication advil = new Medication("advil", 1, 500.00, 2);
+        Medication tylenol = new Medication("tylenol", 2, 500.00, 2);
+        hospital.medications.add(advil);
 
+        Patient p1 = new Patient("patient", "smith", 1, "patient@a.com", "password");
+        p1.prescribedMeds.add(advil);
+        assertTrue(p1.isPrescribed(advil));
+        assertFalse(p1.isPrescribed(tylenol));
     }
 
 }

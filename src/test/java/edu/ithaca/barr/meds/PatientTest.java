0aefdcc8af5968bf9feaf5ffe2e1694ab9e0cffd
Ryan Puorro <rpuorro@ithaca.edu>
2023-04-05 11:11:54 -0400

    wrote requestMedicationTest

--- src/test/java/edu/ithaca/barr/meds/PatientTest.java
+++ src/test/java/edu/ithaca/barr/meds/PatientTest.java
@@ -1,6 +1,7 @@
 package edu.ithaca.barr.meds;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PatientTest {
     
@@ -14,18 +15,28 @@ public class PatientTest {
         hospital.medications.add(tylenol);
 
         Patient p1 = new Patient("patient", "smith", 1, "patient@a.com", "password");
-        p1.requestMedication("advil");
+        p1.requestMedication(advil);
         assertTrue(p1.viewMedication().contains(advil));
+        assertFalse(p1.viewMedication().contains(tylenol));
     }
 
     @Test
     public void requestMedicationTest() {
+        Hospital hospital = new Hospital();
+        Medication advil = new Medication("advil", 1, 500.00, 2);
+        Medication tylenol = new Medication("tylenol", 2, 500.00, 2);
+        hospital.medications.add(advil);
+        hospital.medications.add(tylenol);
 
+        Patient p1 = new Patient("patient", "smith", 1, "patient@a.com", "password");
+        p1.requestMedication(advil);
+        assertTrue(p1.patientMeds.contains(advil));
+        assertFalse(p1.patientMeds.contains(tylenol));
     }
 
     @Test
     public void isPrescribedTest() {
-        
+
     }
 
 }

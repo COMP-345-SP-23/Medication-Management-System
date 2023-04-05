3065d9c572b8e562b7bcbe99fc6c0da7cb16d30f
Ryan Puorro <rpuorro@ithaca.edu>
2023-04-05 11:02:22 -0400

    wrote viewMedicationTest

--- src/test/java/edu/ithaca/barr/meds/PatientTest.java
+++ src/test/java/edu/ithaca/barr/meds/PatientTest.java
@@ -1,5 +1,31 @@
 package edu.ithaca.barr.meds;
 
+import org.junit.jupiter.api.Test;
+
 public class PatientTest {
     
+    @Test
+    public void viewMedicationTest() {
+        //creating a list of available medications
+        Hospital hospital = new Hospital();
+        Medication advil = new Medication("advil", 1, 500.00, 2);
+        Medication tylenol = new Medication("tylenol", 2, 500.00, 2);
+        hospital.medications.add(advil);
+        hospital.medications.add(tylenol);
+
+        Patient p1 = new Patient("patient", "smith", 1, "patient@a.com", "password");
+        p1.requestMedication("advil");
+        assertTrue(p1.viewMedication().contains(advil));
+    }
+
+    @Test
+    public void requestMedicationTest() {
+
+    }
+
+    @Test
+    public void isPrescribedTest() {
+        
+    }
+
 }

fa5dbb5fb2fa6bec1a56cd1d3fc564e0b25f25d3
Ryan Puorro <rpuorro@ithaca.edu>
2023-04-05 11:32:23 -0400

    implemented Patient methods

--- src/main/java/edu/ithaca/barr/meds/Patient.java
+++ src/main/java/edu/ithaca/barr/meds/Patient.java
@@ -1,5 +1,44 @@
 package edu.ithaca.barr.meds;
 
+import java.util.ArrayList;
+
 public class Patient {
     
+    private String firstname;
+    private String lastname;
+    private int id;
+    private String email;
+    private String password;
+    ArrayList<Medication> prescribedMeds;
+    ArrayList<Medication> currentMeds;
+
+    public Patient(String firstname, String lastname, int id, String email, String password) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.prescribedMeds = new ArrayList<>();
+        this.currentMeds = new ArrayList<>();
+    }
+
+    public ArrayList<Medication> viewMedication() {
+        return currentMeds;
+    }
+
+    public void requestMedication(Medication med) {
+        if (prescribedMeds.contains(med)) {
+            currentMeds.add(med);
+        }
+    }
+
+    public boolean isPrescribed(Medication med) {
+        if (prescribedMeds.contains(med)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 }
